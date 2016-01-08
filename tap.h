f6cb5b6c472af345b855b24d612c94562f7baa68
Matthias Puech <puech@cs.mcgill.ca>
2016-01-08 11:26:46 +0100

    Tap: limit buffer size (and therefore time slew)
    Fixes a hard fault with sudden large time differences.
    Should improve performance too! (to test)

--- tap.h
+++ tap.h
@@ -37,6 +37,7 @@ using namespace stmlib;
 namespace mtd 
 {
   const uint8_t kMaxTaps = 1;
+  const uint16_t kMaxBufferSize = 128;
 
   struct TapParameters {
     float time;               /* in samples */
@@ -60,12 +61,16 @@ namespace mtd
       float velocity = tap_params_[tap_nr_].velocity;
       filter_.set_f<FREQUENCY_FAST>(velocity * velocity / 32.0f);
 
-      float time_start = tap_params_[tap_nr_].time + prev_params->time_mod;//* prev_params->scale;
-      float time_end = tap_params_[tap_nr_].time + params->time_mod;//* params->scale;
+      float time_start = tap_params_[tap_nr_].time * prev_params->scale;
+      float time_end = tap_params_[tap_nr_].time * params->scale;
       if (time_start < 0.0f) time_start = 0.0f;
       if (time_end < 0.0f) time_end = 0.0f;
 
       float read_size = size + time_start - time_end;
+
+      if (read_size > kMaxBufferSize) {
+        read_size = kMaxBufferSize;
+      }
 
       MAKE_INTEGRAL_FRACTIONAL(time_start);
       float time = -time_start_fractional; /* why "-"? */
@@ -78,7 +83,7 @@ namespace mtd
 
       /* +1 for interpolation, +1 for rounding to the next */
       uint32_t buf_size = static_cast<uint32_t>(read_size) + 2;
-      int16_t buf[buf_size];
+      int16_t buf[kMaxBufferSize];
 
       buffer_->Read(buf, time_start_integral, buf_size);
 
@@ -88,7 +93,7 @@ namespace mtd
         int16_t b = buf[time_integral + 1];
         float sample = static_cast<float>(a + (b - a) * time_fractional) / 32768.0f;
 
-        /* sample = filter_.Process<FILTER_MODE_LOW_PASS>(sample); */
+        sample = filter_.Process<FILTER_MODE_LOW_PASS>(sample);
         *output += sample * (2.0f - velocity) * velocity;
 
         time += time_increment;
