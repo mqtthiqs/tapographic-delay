3ae273fff280b63a01f8de3a1d1a74907bf3daf2
Matthias Puech <puech@cs.mcgill.ca>
2016-02-26 19:30:20 +0100

    TapAllocator: simplify next_voices_ increment

--- tap_allocator.cc
+++ tap_allocator.cc
@@ -60,7 +60,7 @@ namespace mtd
       taps_[oldest_voice].fade_out(fade_time_);
     }
 
-    next_voice_ = next_voice_ == kMaxTaps-1 ? 0 : next_voice_ + 1;
+    next_voice_ = next_voice_ % kMaxTaps;
   }
 
   void TapAllocator::Remove() {
