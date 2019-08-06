96bf4708370f291c3dd493fc1627da4cb494f488
Matthias Puech <matthias.puech@gmail.com>
2019-08-06 17:07:09 +0200

    LookupTables: cleanup of factory banks

--- resources/lookup_tables.py
+++ resources/lookup_tables.py
@@ -93,25 +93,25 @@ slots = []
 #     ],
 # ]
 
-# Variante 1_1
-base_tapos = [
-    [{'time': 2., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.},
-     {'time': 7., 'velo': 0.5, 'typ': VEL_BP, 'pan': 0.33},
-     {'time': 9., 'velo': 0.75, 'typ': VEL_BP, 'pan': 0.66},
-     {'time': 12., 'velo': 1.0, 'typ': VEL_BP, 'pan': 1.},
-    ],
-    [{'time': 4., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.},
-     {'time': 8., 'velo': 0.75, 'typ': VEL_BP, 'pan': 1.},
-    ],
-    [{'time': 1., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.},
-     {'time': 3., 'velo': 0.5, 'typ': VEL_BP, 'pan': 0.33},
-     {'time': 6., 'velo': 0.75, 'typ': VEL_BP, 'pan': 0.66},
-     {'time': 11., 'velo': 1., 'typ': VEL_BP, 'pan': 1.},
-    ],
-    [{'time': 5., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.},
-     {'time': 10., 'velo': 0.75, 'typ': VEL_BP, 'pan': 1.},
-    ],
-]
+# # Variante 1_1
+# base_tapos = [
+#     [{'time': 2., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.},
+#      {'time': 7., 'velo': 0.5, 'typ': VEL_BP, 'pan': 0.33},
+#      {'time': 9., 'velo': 0.75, 'typ': VEL_BP, 'pan': 0.66},
+#      {'time': 12., 'velo': 1.0, 'typ': VEL_BP, 'pan': 1.},
+#     ],
+#     [{'time': 4., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.},
+#      {'time': 8., 'velo': 0.75, 'typ': VEL_BP, 'pan': 1.},
+#     ],
+#     [{'time': 1., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.},
+#      {'time': 3., 'velo': 0.5, 'typ': VEL_BP, 'pan': 0.33},
+#      {'time': 6., 'velo': 0.75, 'typ': VEL_BP, 'pan': 0.66},
+#      {'time': 11., 'velo': 1., 'typ': VEL_BP, 'pan': 1.},
+#     ],
+#     [{'time': 5., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.},
+#      {'time': 10., 'velo': 0.75, 'typ': VEL_BP, 'pan': 1.},
+#     ],
+# ]
 
 # # Variante 2
 # base_tapos = [
@@ -173,6 +173,48 @@ base_tapos = [
 # ]
 
 
+# Variante 1_3
+base_tapos = [
+     [{'time': 2., 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.1},
+      {'time': 7., 'velo': 0.5, 'typ': VEL_BP, 'pan': 0.},
+      {'time': 9., 'velo': 0.75, 'typ': VEL_BP, 'pan': 0.2},
+      {'time': 12., 'velo': 1.0, 'typ': VEL_BP, 'pan': 0.1},
+     ],
+     [{'time': 4., 'velo': 0.20, 'typ': VEL_BP, 'pan': 0.43},
+      {'time': 8., 'velo': 0.80, 'typ': VEL_BP, 'pan': 0.23},
+     ],
+     [{'time': 1., 'velo': 0.20, 'typ': VEL_BP, 'pan': 0.66},
+      {'time': 3., 'velo': 0.46, 'typ': VEL_BP, 'pan': 0.76},
+      {'time': 6., 'velo': 0.73, 'typ': VEL_BP, 'pan': 0.56},
+      {'time': 11., 'velo': 0.95, 'typ': VEL_BP, 'pan': 0.66},
+     ],
+     [{'time': 5., 'velo': 0.27, 'typ': VEL_BP, 'pan': 0.9},
+      {'time': 10., 'velo': 0.78, 'typ': VEL_BP, 'pan': 1.},
+     ],
+]
+
+
+# # Variante 1_4
+# base_tapos = [
+#      [{'time': 1.95, 'velo': 0.25, 'typ': VEL_BP, 'pan': 0.1},
+#       {'time': 6.81, 'velo': 0.5, 'typ': VEL_BP, 'pan': 0.},
+#       {'time': 9.20, 'velo': 0.75, 'typ': VEL_BP, 'pan': 0.2},
+#       {'time': 12.09, 'velo': 1.0, 'typ': VEL_BP, 'pan': 0.1},
+#      ],
+#      [{'time': 3.86, 'velo': 0.20, 'typ': VEL_BP, 'pan': 0.43},
+#       {'time': 7.76, 'velo': 0.80, 'typ': VEL_BP, 'pan': 0.23},
+#      ],
+#      [{'time': 0.81, 'velo': 0.20, 'typ': VEL_BP, 'pan': 0.66},
+#       {'time': 2.99, 'velo': 0.46, 'typ': VEL_BP, 'pan': 0.76},
+#       {'time': 6.03, 'velo': 0.73, 'typ': VEL_BP, 'pan': 0.56},
+#       {'time': 10.84, 'velo': 0.95, 'typ': VEL_BP, 'pan': 0.66},
+#      ],
+#      [{'time': 5.22, 'velo': 0.27, 'typ': VEL_BP, 'pan': 0.9},
+#       {'time': 9.88, 'velo': 0.78, 'typ': VEL_BP, 'pan': 1.},
+#      ],
+# ]
+
+
 for num in range(16):
     tapo = []
     for bit in range(4):
@@ -185,292 +227,11 @@ for num in range(16):
            'types': map(lambda x: x['typ'], tapo),
            'pans': map(lambda x: x['pan'], tapo)})
 
-#############
+# complete slots with empty tapos
 
-# Bank A: long bouncings
+while(len(slots) < 24):
+    slots.append({ 'size': 0, 'times': [], 'velos': [], 'types': [], 'pans': []})
 
-# 1
-length = 5 # seconds
-size = 16 # taps
-
-t = normalize(np.add.accumulate(np.power(1.2, np.arange(1.0, size+1))))
-
-slots.append({
-    'size': size,
-    'times': t*sample_rate*length,
-    'velos': normalize(np.arange(1.0, size+1)),
-    'types': np.repeat([VEL_LP], size),
-    'pans': np.resize([0,1], size),
-})
-
-# 2
-length = 6
-t = normalize(np.add.accumulate(np.power(0.9, np.arange(1.0, size+1))))
-
-slots.append({
-    'size': size,
-    'times': t*sample_rate*length,
-    'velos': 1-normalize(np.arange(0.0, size)),
-    'types': np.repeat([VEL_BP], size),
-    'pans': np.resize([0,1], size),
-})
-
-# 3
-length = 5
-size = 30
-
-t1 = normalize(np.power(1.3, np.arange(1.0, 16)))
-t2 = t1[::-1]
-t = normalize(np.add.accumulate(np.append(t1, t2)))
-
-v1 = np.arange(1.0, 16)
-v2 = v1[::-1]
-v = normalize(np.append(v1, v2))
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * length,
-    'velos': v * v,
-    'types': np.repeat([VEL_AMP], size),
-    'pans': normalize(np.arange(0.0, size)),
-})
-
-# 4
-length = 5
-size = 30
-
-t1 = normalize(np.power(0.8, np.arange(1.0, size/2+1)))
-t2 = t1[::-1]
-t = normalize(np.add.accumulate(np.append(t1, t2)))
-
-v1 = np.arange(1.0, size/2+1)
-v2 = v1[::-1]
-v = 1 - normalize(np.append(v1, v2))
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * length,
-    'velos': v,
-    'types': np.repeat([VEL_BP], size),
-    'pans': 1-normalize(np.arange(0.0, size)),
-})
-
-# 5
-length = 3
-size = 16
-
-t = normalize(np.add.accumulate(np.power(0.99, np.arange(1.0, size+1))))
-v = normalize(np.power(1.2, np.arange(1.0, size+1)))
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * length,
-    'velos': v,
-    'types': np.repeat([VEL_AMP], size),
-    'pans': np.random.uniform(size=size),
-})
-
-# 6
-length = 3
-size = 16
-
-t = normalize(np.add.accumulate(np.power(1.005, np.arange(1.0, size+1))))
-v1 = normalize(np.power(0.6, np.arange(1.0, size/2)))
-v2 = v1[::-1]
-v = normalize(np.append(v1, v2))
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * length,
-    'velos': v,
-    'types': np.repeat([VEL_LP], size),
-    'pans': normalize(np.arange(0.0, size)),
-})
-
-# Bank B:
-
-# 1
-t = [0,1,0,2,1,0,0,3,0]
-t = np.array(map(float, t))
-t = normalize(np.add.accumulate(t+1))
-v = [0.1,1,0.3,0.1,1,0.3,1,0.3,0.1]
-size = len(t)
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * size / 4,
-    'velos': v,
-    'types': np.repeat([VEL_LP], size),
-    'pans': np.resize([0,1], size),
-})
-
-# 2
-t = [1,0,0,0,3,2,0,1,1,0,1,0]
-t = np.array(map(float, t))
-t = normalize(np.add.accumulate(t+1))
-v = [0.1,0.3,0.7,0.55,0.2,0.5,0.9,0.45,0.6,0.1,0.9,0.2]
-size = len(t)
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * size / 4,
-    'velos': v,
-    'types': np.repeat([VEL_BP], size),
-    'pans': np.resize([0,1], size),
-})
-
-# 3
-t = [1,1,1,1,0,0,0,0,4,4,4,4,2,2,2,2]
-t = np.array(map(float, t))
-t = normalize(np.add.accumulate(t+1))
-v = [0.5,0.5,1,1,1,1,0.5,0.5,0.5,0.5,1,1,1,1,1,1]
-size = len(t)
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * size / 4,
-    'velos': v,
-    'types': np.repeat([VEL_AMP], size),
-    'pans': np.resize([0,1], size),
-})
-
-# 4
-t = [0,0,0,0,1,1,1,1,2,2,2,2,4,4,4,4]
-t = np.array(map(float, t))
-t = normalize(np.add.accumulate(t+1))
-v = np.resize([0.35,0.5,0.65,0.8], size)
-size = len(t)
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * size / 2,
-    'velos': v,
-    'types': np.repeat([VEL_BP], size),
-    'pans': np.resize([0,1], size),
-})
-
-# 5
-t = [2,2,2,0,0,0,1,1,1,0,0,0,3,3,3]
-t = np.array(map(float, t))
-t = normalize(np.add.accumulate(t+1))
-v = np.resize([0,0.5,1], size)
-size = len(t)
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * size / 4,
-    'velos': v,
-    'types': np.repeat([VEL_LP], size),
-    'pans': np.resize([0,1], size),
-})
-
-# 6
-t = [0,0,0,2,0,0,0,4,0,0,0,8,0,0,0]
-t = np.array(map(float, t))
-t = normalize(np.add.accumulate(t+1))
-v = np.resize([0,0.33, 0.66,1], size)
-size = len(t)
-
-slots.append({
-    'size': size,
-    'times': t * sample_rate * size / 4,
-    'velos': v,
-    'types': np.repeat([VEL_AMP], size),
-    'pans': np.resize([0,1], size),
-})
-
-# Bank C: Random / diffusion IRs
-
-phi = 1.61803398875
-
-# 1: golden ratio
-slots.append({
-    'size': 3,
-    'times': np.array([1, phi, phi * phi]) * sample_rate * 1,
-    'velos': [1, 1, 1],
-    'types': np.repeat([VEL_AMP], 3),
-    'pans': [0, 1, 0.5],
-})
-
-# 2: optimally diffusing 6-taps
-slots.append({
-    'size': 6,
-    'times': np.array([0.11772496, 0.30086703, 0.398813414, 0.545568,
-                       0.750073, 1.0]) * sample_rate * 4,
-    'velos': [1, 1./2, 1./3, 1./4, 1./5, 1./6],
-    'types': np.repeat([VEL_LP], 6),
-    'pans': [0, 1, 0, 1, 0, 1],
-})
-
-# 3: crescendo with random times
-size = 16
-
-ts = np.random.uniform(size=size)
-ts = normalize(np.add.accumulate(ts))
-vs = ts
-
-slots.append({
-    'size': size,
-    'times': ts * sample_rate * 3,
-    'velos': vs,
-    'types': np.repeat([VEL_BP], size),
-    'pans': np.random.uniform(size=size),
-})
-
-# 4: decrescendo with random times
-ts = np.random.uniform(size=size)
-ts = normalize(np.add.accumulate(ts))
-vs = ts
-
-slots.append({
-    'size': size,
-    'times': ts * sample_rate * 3,
-    'velos': 1-vs,
-    'types': np.repeat([VEL_AMP], size),
-    'pans': np.random.uniform(size=size),
-})
-
-# 5:
-ts = np.random.uniform(size=size)
-ts = normalize(np.sort(ts))
-vs = np.random.uniform(size=size)
-
-slots.append({
-    'size': size,
-    'times': ts * sample_rate * 5,
-    'velos': vs,
-    'types': np.repeat([VEL_BP], size),
-    'pans': normalize(np.arange(0.0, size)),
-})
-
-# 6: all random
-slots.append({
-    'size': size,
-    'times': np.random.uniform(size=size) * sample_rate * 3,
-    'velos': np.random.uniform(size=size),
-    'types': np.random.randint(0, high=3, size=size),
-    'pans': np.random.uniform(size=size),
-})
-
-# Bank D:
-# (left intentionally empty)
-slots.append({ 'size': 0, 'times': [], 'velos': [], 'types': [], 'pans': []})
-slots.append({ 'size': 0, 'times': [], 'velos': [], 'types': [], 'pans': []})
-slots.append({ 'size': 0, 'times': [], 'velos': [], 'types': [], 'pans': []})
-slots.append({ 'size': 0, 'times': [], 'velos': [], 'types': [], 'pans': []})
-slots.append({ 'size': 0, 'times': [], 'velos': [], 'types': [], 'pans': []})
-slots.append({ 'size': 0, 'times': [], 'velos': [], 'types': [], 'pans': []})
-
-# # Graph all IRs:
-# import matplotlib.pyplot as plt
-
-# for i in range(len(slots)):
-#     plt.figure(figsize=(10,7))
-#     slot = slots[i]
-#     s = zip(slot['times'], slot['velos'])
-#     for (x,y) in s:
-#         plt.axvline(x, ymax=y)
-#     plt.savefig("slot"+str(i)+".pdf")
 
 # Formatting and writing to table
 
